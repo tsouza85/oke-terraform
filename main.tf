6dddabab498d7b3f40db10867bf2960ae6df79fd
Tiago Souza <tiago.souza@gmail.com>
2023-07-12 09:22:43 -0300

    refact

--- main.tf
+++ main.tf
@@ -1,24 +1,6 @@
-terraform {
-  required_providers {
-    oci = {
-      source  = "oracle/oci"
-      version = "4.96.0"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 1.13.0"
-    }
-
-    k8s = {
-      source  = "banzaicloud/k8s"
-      version = ">= 0.8.0"
-    }
-  }
-}
-
-# == Provider ==
-
+################################################################################
+# Provider
+################################################################################
 provider "oci" {
   region       = var.region
   tenancy_ocid = var.tenancy_ocid
@@ -36,10 +18,44 @@ provider "k8s" {
   config_path = "${path.module}/generated/kubeconfig"
 }
 
-# == Compartment ==
+################################################################################
+# Compartment
+################################################################################
 
 resource "oci_identity_compartment" "k8s" {
   compartment_id = var.tenancy_ocid
   description    = "Compartment for Free Tier K8s Cluster"
   name           = "OracleManagedKubernetesCompartment"
+}
+
+################################################################################
+# OKE
+################################################################################
+
+module "oke" {
+  source = "oracle-terraform-modules/oke/oci"
+
+  compartment_id = oci_identity_compartment.k8s.id
+
+  region                      = var.region
+  home_region                 = var.region
+  tenancy_id                  = var.tenancy_ocid
+  create_operator             = false
+  create_bastion_host         = false
+  control_plane_type          = "public"
+  control_plane_allowed_cidrs = ["0.0.0.0/0"]
+  kubernetes_version          = "v1.24.1"
+
+  node_pools = {
+    np1 = {
+      boot_volume_size = 50
+      node_pool_size   = 2
+      ocpus            = 2
+      shape            = "VM.Standard.A1.Flex"
+      memory           = 12
+    }
+  }
+  providers = {
+    oci.home = oci
+  }
 }
---
+++ versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = ">=4.96.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 1.13.0"
+    }
+
+    k8s = {
+      source  = "banzaicloud/k8s"
+      version = ">= 0.8.0"
+    }
+  }
+}
